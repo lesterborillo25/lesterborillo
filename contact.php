0cc3c78b9ce044b07e0cae87c2fee96cfcbf9f82
lesterborillo25 <lesterborillo@outlook.com>
2024-08-02 09:36:17 +0800

    tes
    sadas

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
+$field_subject = $_POST['cf_subject'];
 $field_message = $_POST['cf_message'];
 
 $mail_to = 'lesterborillo@outlook.com';
@@ -8,6 +9,7 @@ $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'E-mail: '.$field_subject."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$field_email."\r\n";
